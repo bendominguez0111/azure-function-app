dc23231c84f74f275defa6b7404bf32f50f91199
Ben Dominguez <ben@fantasyfootballdatapros.com>
2023-07-27 11:11:20 -0400

    added logic to timer example

--- TimerExample/__init__.py
+++ TimerExample/__init__.py
@@ -1,6 +1,9 @@
 import datetime
 import logging
 
+import pandas as pd
+import nfl_data_py as nfl
+
 import azure.functions as func
 
 
@@ -12,3 +15,10 @@ def main(mytimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
+    # get the top 5 players by air yards in 2022
+    df = nfl.import_pbp_data(years=[2022])
+    air_yards = df.groupby(['receiver_player_id', 'receiver_player_name'], as_index=False)\
+        ['air_yards'].sum().sort_values(by='air_yards', ascending=False).head(5)
+    
+    print(air_yards)
